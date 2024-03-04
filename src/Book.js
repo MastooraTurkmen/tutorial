80b1eb4342707af49b5fa7e5c1001e1fcb3219ea
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-04 18:24:49 +0430

    Book set up

---
+++ src/Book.js
@@ -0,0 +1,11 @@
+const Book = (props) => {
+    const { img, title, author } = props.book
+    return (
+        <article className="book">
+            <img src={img} />
+            <h1>{title}</h1>
+            <h4>{author}</h4>
+            <button type="button" onClick={() => { console.log(title) }}>Click More</button>
+        </article>
+    )
+}
