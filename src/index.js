851b4a0d4c07d11cd7a497dfc0b953e6f10e8a24
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-04 13:29:39 +0430

    book style project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function AmazonBooks() {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
+
+const Book = () => {
+  return (
+    <>
+      <Title />
+      <Image />
+      <Author />
+    </>
+  )
+}
+
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81mpSoJzv4L._AC_UL254_SR254,254_.jpg" alt="" />
+const Title = () => <h1>I Love You to The Moon and Back</h1>
+const Author = () => <h4>John Deo</h4>
+
+ReactDOM.render(<AmazonBooks />, document.getElementById('root'))
