1f0f034883004494139fa45da6a0eede23f2b9f2
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-04 13:55:44 +0430

    style inline

--- src/index.js
+++ src/index.js
@@ -29,6 +29,10 @@ const Book = () => {
 
 const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81mpSoJzv4L._AC_UL254_SR254,254_.jpg" alt="" />
 const Title = () => <h1>I Love You to The Moon and Back</h1>
-const Author = () => <h4>John Deo</h4>
+const Author = () => <h4 style={{
+  color: "#617d98",
+  fontSize: "0.78rem",
+  marginTop: "0.25rem"
+}} >John Deo</h4>
 
 ReactDOM.render(<AmazonBooks />, document.getElementById('root'))
