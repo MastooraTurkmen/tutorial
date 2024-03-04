aeca965a38b3c0799086091fd7786b6f7eddbfa3
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-04 17:47:02 +0430

    add more books

--- src/index.js
+++ src/index.js
@@ -6,15 +6,25 @@ import './style.css'
 
 const books = [
   {
+    img: "https://m.media-amazon.com/images/I/91afIYwE9BL._AC_UY327_FMwebp_QL65_.jpg",
+    title: "The Exchange: After The Firm",
+    author: "John Grisham"
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/81cc7Zt6fvL._AC_UY327_FMwebp_QL65_.jpg",
+    title: "The Secret: A Jack Reacher Novel",
+    author: "Lee Child"
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/81O4b-j4vnL._AC_UY327_FMwebp_QL65_.jpg",
+    title: "First Lie Wins: A Novel",
+    author: "Ashley Elston"
+  },
+  {
     img: "https://images-na.ssl-images-amazon.com/images/I/81mpSoJzv4L._AC_UL254_SR254,254_.jpg",
     title: "I Love You to The Moon and Back",
     author: "Amelia Hepworth"
   },
-  {
-    img: "https://images-na.ssl-images-amazon.com/images/I/913C+MR3S5L._AC_UL127_SR127,127_.jpg",
-    title: "The Women: A Novel",
-    author: "Ameliy Brown"
-  }
 ]
 
 function AmazonBooks() {
