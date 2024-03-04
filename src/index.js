17f094fc09153215ad937fdf6a8822136843d4e8
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-04 18:23:15 +0430

    books data

--- src/index.js
+++ src/index.js
@@ -2,35 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './style.css'
 
-
-
-const books = [
-  {
-    id: 1,
-    img: "https://m.media-amazon.com/images/I/91afIYwE9BL._AC_UY327_FMwebp_QL65_.jpg",
-    title: "The Exchange: After The Firm",
-    author: "John Grisham"
-  },
-  {
-    id: 2,
-    img: "https://m.media-amazon.com/images/I/81cc7Zt6fvL._AC_UY327_FMwebp_QL65_.jpg",
-    title: "The Secret: A Jack Reacher Novel",
-    author: "Lee Child"
-  },
-  {
-    id: 3,
-    img: "https://m.media-amazon.com/images/I/81O4b-j4vnL._AC_UY327_FMwebp_QL65_.jpg",
-    title: "First Lie Wins: A Novel",
-    author: "Ashley Elston"
-  },
-  {
-    id: 4,
-    img: "https://images-na.ssl-images-amazon.com/images/I/81mpSoJzv4L._AC_UL254_SR254,254_.jpg",
-    title: "I Love You to The Moon and Back",
-    author: "Amelia Hepworth"
-  },
-]
-
 function AmazonBooks() {
   return (
     <section className="booklist">{
