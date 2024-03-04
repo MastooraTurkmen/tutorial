c20bd2b1056a9d485e2ad55bc7f34b199f833fb6
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-04 17:48:19 +0430

    adds id

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ function AmazonBooks() {
   return (
     <section className="booklist">{
       books.map((book) => {
-        return <Book book={book} />
+        return <Book key={book.id} book={book} />
       })
     }
     </section>
